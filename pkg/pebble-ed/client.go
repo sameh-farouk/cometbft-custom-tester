1ae2f3dfc18aa1b64b08c487750476df262aca43
nabil salah <nabil.salah203@gmail.com>
2025-03-19 15:17:31 +0200

    feat: add sign
    Signed-off-by: nabil salah <nabil.salah203@gmail.com>

--- pkg/pebble-ed/client.go
+++ pkg/pebble-ed/client.go
@@ -2,7 +2,6 @@ package pebble_ed
 
 import (
 	"crypto/ed25519"
-	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"math/rand"
@@ -46,12 +45,8 @@ var _ loadtest.ClientFactory = (*MyABCIAppClientFactory)(nil)
 // each client will be responsible for maintaining its own state in a
 // thread-safe manner.
 type MyABCIAppClient struct{
-	keyPrefix    []byte // Contains the client ID
-	keySuffixLen int
-	valueLen     int
-	priKey		ed25519.PrivateKey
-	pubKey		ed25519.PublicKey
-	txCounter uint64
+	batchSize int
+	txCounter int
 	senders   []string
 }
 
@@ -76,25 +71,10 @@ func (f *MyABCIAppClientFactory) ValidateConfig(cfg loadtest.Config) error {
 }
 
 func (f *MyABCIAppClientFactory) NewClient(cfg loadtest.Config) (loadtest.Client, error) {
-	keyPrefix := []byte(randStr(KVStoreClientIDLen))
-	keySuffixLen, err := requiredKVStoreSuffixLen(cfg.MaxTxsPerEndpoint())
-	if err != nil {
-		return nil, err
-	}
-	keyLen := len(keyPrefix) + keySuffixLen
-	// value length = key length - (32 bytes + 64 bytes)(public key + signature) (to cater for "=" symbol)
-	valueLen := cfg.Size - keyLen - (32+64)
-	pub, priv, err := ed25519.GenerateKey(nil)
-	if err != nil {
-		return nil, err
-	}
+	batchSize := cfg.Size
 	senderIds := []string{"1", "2", "3", "4"}
 	return &MyABCIAppClient{
-		keyPrefix:    keyPrefix,
-		keySuffixLen: keySuffixLen,
-		valueLen:     valueLen,
-		priKey: 	priv,
-		pubKey:		pub,
+		batchSize: batchSize,
 		txCounter: 0,
 		senders:   senderIds,
 	}, nil
@@ -114,30 +94,24 @@ func requiredKVStoreSuffixLen(maxTxCount uint64) (int, error) {
 }
 
 var privateKeyMap = map[string]string{
-    "1": "1111111111111111111111111111111111111111111111111111111111111111" ,
-    "2": "2222222222222222222222222222222222222222222222222222222222222222",
-    "3": "3333333333333333333333333333333333333333333333333333333333333333" ,
-    "4": "4444444444444444444444444444444444444444444444444444444444444444",
+    "1": "23e980b97c67af9b94319b6672049fbd2f9992eaf6a567a2b5a66286e527e8e9c8af5ee74756bb934c9c3f93a3ffa4125c93d8a76619a1834f4511334d83d45f" ,
+    "2": "11a2070b5bf25002c43d238117840fb97492266d3e0fb7637b069d5569b5d8283382d764d3e30ce4c3aab066335a558e8f632d2aaf161e6aa5615c57176cfbca",
+    "3": "376384a0c4d4ef4e95bf980acea6ec6d7b8bbdaa06d91ca68383d018e885dda204c01c7d4f6c784504fce83f97968145e8aa6ca461ec19f3a685466152f17644" ,
+    "4": "7403b7706deba2d8d036d00c5e1e087542fff733b1b3f1b776bf2fa64bcd5d98d06a22ce4b7a59ceac3a898504901f41e27491ed3cc90e8ee46ac43e9305d61a",
 }
 
 type Transfer struct {
-    Id        uint64 `json:"id"`
+    Id        string `json:"id"`
     Sender    string `json:"sender"`
     Dest      string `json:"dest"`
     Amount    string `json:"amount"`
     Signature string `json:"signature"`
 }
 
-// Transaction struct matching the application's expected format
-type Transaction struct {
-    Transfers []Transfer `json:"transfers"`
-}
 
 func (t *Transfer) Challenge() []byte {
     challenge := []byte{}
-    numBytes := make([]byte, 8)
-    binary.BigEndian.PutUint64(numBytes, t.Id)
-    challenge = append(challenge, numBytes...)
+    challenge = append(challenge, []byte(t.Id)...)
     challenge = append(challenge, []byte("=")...)
     challenge = append(challenge, []byte(t.Sender)...)
     challenge = append(challenge, []byte("=")...)
@@ -152,54 +126,52 @@ func (t *Transfer) Challenge() []byte {
 // loadtest package, so don't worry about that. Only return an error here if you
 // want to completely fail the entire load test operation.
 func (c *MyABCIAppClient) GenerateTx() ([]byte, error) {
-    c.txCounter++
-    
-    senderIdx := rand.Intn(len(c.senders))
-    sender := c.senders[senderIdx]
-    
-    var dest string
-    for {
-        destIdx := rand.Intn(len(c.senders))
-        dest = c.senders[destIdx]
-        if dest != sender {
-            break
-        }
-    }
-    
-    amount := rand.Intn(500) + 1
-    
-    transfer := Transfer{
-        Id:     c.txCounter,
-        Sender: sender,
-        Dest:   dest,
-        Amount: strconv.Itoa(amount),
-    }
-    
-    challenge := transfer.Challenge()
-    
-    privKeyHex := privateKeyMap[sender]
-    privKeyBytes, err := hex.DecodeString(privKeyHex)
-    if err != nil {
-        return nil, fmt.Errorf("failed to decode private key: %v", err)
-    }
-    
-    privateKey := ed25519.PrivateKey(privKeyBytes)
-    signature := ed25519.Sign(privateKey, challenge)
-    
-    transfer.Signature = hex.EncodeToString(signature)
-    
-    txData := fmt.Sprintf("%s=%s=%s=%s=%s", 
-        formatUint64(transfer.Id),
-        transfer.Sender, 
-        transfer.Dest, 
-        transfer.Amount, 
-        transfer.Signature)
-    
+	n := c.batchSize
+	txData := ""
+	for range n {
+		c.txCounter++
+		senderIdx := rand.Intn(len(c.senders))
+		sender := c.senders[senderIdx]
+		
+		var dest string
+		for {
+			destIdx := rand.Intn(len(c.senders))
+			dest = c.senders[destIdx]
+			if dest != sender {
+				break
+			}
+		}
+		
+		amount := rand.Intn(500) + 1
+		
+		transfer := Transfer{
+			Id:     strconv.Itoa(c.txCounter),
+			Sender: sender,
+			Dest:   dest,
+			Amount: strconv.Itoa(amount),
+		}
+		
+		challenge := transfer.Challenge()
+		
+		privKeyHex := privateKeyMap[sender]
+		privKeyBytes, err := hex.DecodeString(privKeyHex)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode private key: %v", err)
+		}
+		
+		privateKey := ed25519.PrivateKey(privKeyBytes)
+		signature := ed25519.Sign(privateKey, challenge)
+		
+		transfer.Signature = hex.EncodeToString(signature)
+		txData += fmt.Sprintf("%s=%s=%s=%s=%s", 
+			transfer.Id,
+			transfer.Sender, 
+			transfer.Dest, 
+			transfer.Amount, 
+			transfer.Signature)
+			txData += ":"
+	}
+	txData = txData[:len(txData)-1]
+    fmt.Println(txData)
     return []byte(txData), nil
-}
-
-func formatUint64(n uint64) string {
-    bytes := make([]byte, 8)
-    binary.BigEndian.PutUint64(bytes, n)
-    return string(bytes)
 }
