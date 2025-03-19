e76dad28a8d7f9da5d31e676ed7b02887e13c34b
Sameh Abouel-saad <samehabouelsaad@gmail.com>
2025-03-19 23:39:21 +0200

    clean code

--- pkg/pebble-ed/client.go
+++ pkg/pebble-ed/client.go
@@ -10,30 +10,6 @@ import (
 	"github.com/cometbft/cometbft-load-test/pkg/loadtest"
 )
 
-const (
-	KVStoreClientIDLen int = 5 // Allows for 6,471,002 random client IDs (62C5)
-	kvstoreMinValueLen int = 1 // We at least need 1 character in a key/value pair's value.
-)
-
-var kvstoreMaxTxsByKeySuffixLen = []uint64{
-	0,              // 0
-	62,             // 1
-	1891,           // 2
-	37820,          // 3
-	557845,         // 4
-	6471002,        // 5
-	61474519,       // 6
-	491796152,      // 7
-	3381098545,     // 8
-	20286591270,    // 9
-	107518933731,   // 10
-	508271323092,   // 11
-	2160153123141,  // 12
-	8308281242850,  // 13
-	29078984349975, // 14
-	93052749919920, // 15
-}
-
 // MyABCIAppClientFactory creates instances of MyABCIAppClient
 type MyABCIAppClientFactory struct{}
 
@@ -44,7 +20,7 @@ var _ loadtest.ClientFactory = (*MyABCIAppClientFactory)(nil)
 // will be created per connection to the remote CometBFT RPC endpoint, and
 // each client will be responsible for maintaining its own state in a
 // thread-safe manner.
-type MyABCIAppClient struct{
+type MyABCIAppClient struct {
 	batchSize int
 	txCounter int
 	senders   []string
@@ -79,30 +55,29 @@ func (f *MyABCIAppClientFactory) NewClient(cfg loadtest.Config) (loadtest.Client
 }
 
 var privateKeyMap = map[string]string{
-    "1": "23e980b97c67af9b94319b6672049fbd2f9992eaf6a567a2b5a66286e527e8e9c8af5ee74756bb934c9c3f93a3ffa4125c93d8a76619a1834f4511334d83d45f" ,
-    "2": "11a2070b5bf25002c43d238117840fb97492266d3e0fb7637b069d5569b5d8283382d764d3e30ce4c3aab066335a558e8f632d2aaf161e6aa5615c57176cfbca",
-    "3": "376384a0c4d4ef4e95bf980acea6ec6d7b8bbdaa06d91ca68383d018e885dda204c01c7d4f6c784504fce83f97968145e8aa6ca461ec19f3a685466152f17644" ,
-    "4": "7403b7706deba2d8d036d00c5e1e087542fff733b1b3f1b776bf2fa64bcd5d98d06a22ce4b7a59ceac3a898504901f41e27491ed3cc90e8ee46ac43e9305d61a",
+	"1": "23e980b97c67af9b94319b6672049fbd2f9992eaf6a567a2b5a66286e527e8e9c8af5ee74756bb934c9c3f93a3ffa4125c93d8a76619a1834f4511334d83d45f",
+	"2": "11a2070b5bf25002c43d238117840fb97492266d3e0fb7637b069d5569b5d8283382d764d3e30ce4c3aab066335a558e8f632d2aaf161e6aa5615c57176cfbca",
+	"3": "376384a0c4d4ef4e95bf980acea6ec6d7b8bbdaa06d91ca68383d018e885dda204c01c7d4f6c784504fce83f97968145e8aa6ca461ec19f3a685466152f17644",
+	"4": "7403b7706deba2d8d036d00c5e1e087542fff733b1b3f1b776bf2fa64bcd5d98d06a22ce4b7a59ceac3a898504901f41e27491ed3cc90e8ee46ac43e9305d61a",
 }
 var edPrivateKeyMap = map[string]ed25519.PrivateKey{}
-type Transfer struct {
-    Id        string `json:"id"`
-    Sender    string `json:"sender"`
-    Dest      string `json:"dest"`
-    Amount    string `json:"amount"`
-    Signature string `json:"signature"`
-}
 
+type Transfer struct {
+	Id        string `json:"id"`
+	Sender    string `json:"sender"`
+	Dest      string `json:"dest"`
+	Amount    string `json:"amount"`
+	Signature string `json:"signature"`
+}
 
 func (t *Transfer) Challenge() []byte {
-    challenge := []byte{}
-    challenge = append(challenge, []byte(t.Id)...)
-    challenge = append(challenge, []byte(t.Sender)...)
-    challenge = append(challenge, []byte(t.Dest)...)
-    challenge = append(challenge, []byte(t.Amount)...)
-    return challenge
+	challenge := []byte{}
+	challenge = append(challenge, []byte(t.Id)...)
+	challenge = append(challenge, []byte(t.Sender)...)
+	challenge = append(challenge, []byte(t.Dest)...)
+	challenge = append(challenge, []byte(t.Amount)...)
+	return challenge
 }
-
 
 // GenerateTx must return the raw bytes that make up the transaction for your
 // ABCI app. The conversion to base64 will automatically be handled by the
@@ -115,35 +90,35 @@ func (c *MyABCIAppClient) GenerateTx() ([]byte, error) {
 		c.txCounter++
 		senderIdx := rand.Intn(len(c.senders))
 		sender := c.senders[senderIdx]
-		
+
 		var dest string
 		destIdx := rand.Intn(len(c.senders))
 		dest = c.senders[destIdx]
-		
+
 		amount := 1
-		
+
 		transfer := Transfer{
 			Id:     strconv.Itoa(c.txCounter),
 			Sender: sender,
 			Dest:   dest,
 			Amount: strconv.Itoa(amount),
 		}
-		
+
 		challenge := transfer.Challenge()
-		
+
 		privateKey := edPrivateKeyMap[sender]
 		signature := ed25519.Sign(privateKey, challenge)
-		
+
 		transfer.Signature = hex.EncodeToString(signature)
-		txData += fmt.Sprintf("%s=%s=%s=%s=%s", 
+		txData += fmt.Sprintf("%s=%s=%s=%s=%s",
 			transfer.Id,
-			transfer.Sender, 
-			transfer.Dest, 
-			transfer.Amount, 
+			transfer.Sender,
+			transfer.Dest,
+			transfer.Amount,
 			transfer.Signature)
 		if i != n-1 {
 			txData += ":"
 		}
 	}
-    return []byte(txData), nil
+	return []byte(txData), nil
 }
